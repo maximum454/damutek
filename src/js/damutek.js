edfbda92d804f4544763e9bac6d7bbaf5cddb1af
maxim <karachyov@musan.kz>
2018-04-16 16:15:14 +0600

    form

--- src/js/damutek.js
+++ src/js/damutek.js
@@ -1,9 +1,20 @@
 //= partials/jquery.min.js
 //= partials/slick.min.js
+//= partials/jquery.arcticmodal-0.3.min.js
 
 $(function () {
     $('.js-like').on('click', function () {
         $(this).toggleClass('active');
+    });
+
+    $('.popap_box').click(function() {
+        $('#modalbox_' + $(this).data('body')).arcticmodal({
+            overlay: {
+                css: {
+                    backgroundColor: '#000'
+                }
+            }
+        });
     });
 
     $('.js-reviews').slick({
