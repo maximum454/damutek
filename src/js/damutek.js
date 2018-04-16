4a12759771d4f37b211410c60be6a41b81c9b826
maxim <karachyov@musan.kz>
2018-04-16 11:32:48 +0600

    footer

--- src/js/damutek.js
+++ src/js/damutek.js
@@ -1,6 +1,29 @@
 //= partials/jquery.min.js
+//= partials/slick.min.js
 
 $(function () {
-
+    $('.js-reviews').slick({
+        dots: false,
+        arrows: true,
+        infinite: true,
+        mobileFirst: false,
+        speed: 600,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: "unslick"
+            },
+            {
+                breakpoint: 425,
+                settings: {
+                    dots: true,
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
 });
 
