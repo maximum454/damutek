0e86d769e9c0ead9899a5bdf255e0a192200b068
maxim <maximum454@gmail.com>
2018-11-27 10:55:45 +0600

    passing-test.html

--- src/js/damutek.js
+++ src/js/damutek.js
@@ -8,6 +8,7 @@
 //= partials/jquery.formstyler.js
 //= partials/step-form.js
 
+
 $(function () {
     /*link*/
     $("a[href='#']").click(function () {
@@ -22,9 +23,12 @@ $(function () {
         $(this).toggleClass('active');
     });
 
-    $('.js-accardion').on('click', function () {
-        $(this).toggleClass('active');
-    });
+    if ($('.js-accardion').length) {
+        $('.js-accardion').on('click', function () {
+            $(this).toggleClass('active');
+        });
+    }
+
 
     $('.popap_box').click(function() {
         $('#modalbox_' + $(this).data('body')).arcticmodal({
@@ -36,55 +40,66 @@ $(function () {
         });
     });
 
-    $('.js-reviews').slick({
-        dots: false,
-        arrows: true,
-        infinite: true,
-        mobileFirst: false,
-        speed: 600,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        responsive: [
-            {
-                breakpoint: 768,
-                settings: "unslick"
-            },
-            {
-                breakpoint: 425,
-                settings: {
-                    dots: true,
-                    slidesToShow: 1,
-                    slidesToScroll: 1
+    if ($('.js-reviews').length) {
+        $('.js-reviews').slick({
+            dots: false,
+            arrows: true,
+            infinite: true,
+            mobileFirst: false,
+            speed: 600,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            responsive: [
+                {
+                    breakpoint: 768,
+                    settings: "unslick"
+                },
+                {
+                    breakpoint: 425,
+                    settings: {
+                        dots: true,
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
                 }
-            }
-        ]
-    });
+            ]
+        });
+    }
+
 
     $('.js-star').rating();
 
     $('input[type=checkbox],input[type=file], select').styler();
-    
-    $('.js-select').on('click', function (e) {
-        e.preventDefault();
-        $('.basket__item').each(function() {
-            if ( $('.basket__check').attr('checked')) {
+
+    if ($('.js-select').length) {
+        $('.js-select').on('click', function (e) {
+            e.preventDefault();
+            $('.basket__item').each(function() {
+                if ( $('.basket__check').attr('checked')) {
+                    $('.basket__check').attr('checked', false).trigger('refresh');
+                } else {
+                    $('.basket__check').attr('checked', 'checked').trigger('refresh');
+                }
+            });
+        });
+    }
+
+
+    if ($('.js-delet-all').length) {
+        $('.js-delet-all').on('click',function (e) {
+            e.preventDefault();
+            $('.select-all').attr('checked', false).trigger('refresh');
+            $('.basket__item').each(function() {
                 $('.basket__check').attr('checked', false).trigger('refresh');
-            } else {
-                $('.basket__check').attr('checked', 'checked').trigger('refresh');
-            }
+            });
         });
-    });
+    }
 
-    $('.js-delet-all').on('click',function (e) {
-        e.preventDefault();
-        $('.select-all').attr('checked', false).trigger('refresh');
-        $('.basket__item').each(function() {
-            $('.basket__check').attr('checked', false).trigger('refresh');
+    if ($('.js-filter-btn').length) {
+        $('.js-filter-btn').on('click', function () {
+            $('.filter__panel').slideToggle();
         });
-    });
+    }
 
-    $('.js-filter-btn').on('click', function () {
-        $('.filter__panel').slideToggle();
-    });
 });
 
